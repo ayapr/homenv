4d55fc80959a1672d2ffd5dcdb23364a2c7d37ed
AYapryntsev <a.yapryntsev@documatic.ru>
2016-10-14 15:49:53 +0300

    user input

--- sys_diff.py
+++ sys_diff.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
 import os
 
-print("Hello")
-os.system('ls -la | less')
+print("Python test: user input")
+os.system('mkdir _out')
+os.system('touch _out/diff.patch')
+os.system('diff -u ~/.vimrc ~/_PRJ/my/sys/vim/.vimrc >> _out/diff.patch')
+os.system('vim _out/diff.patch')
+decision = input("Remove temp dir? (y/n): ")
+if decision == 'y' or decision == 'Y':
+    os.system('rm -rf _out')
+print("exit")
