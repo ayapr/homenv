fb84e42c712493440bb119a50094e71e81a0d4f5
AYapryntsev <a.yapryntsev@documatic.ru>
2016-11-07 18:00:38 +0300

    hico init

---
+++ sample/neon.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+class Example(object):
+
+    backquotes = `backquotes`
+
+    @memoize(size=10)        
+    def Call(self, param1=None):
+        u'''unicode'''
+        return param1 + 10 * 10
+
+    def Call2(self):
+        b'''bytes'''
+        # Comment
+        return self.Call(param1=10)
